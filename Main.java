4812e513d59ad4e046ae610891f54079b1297a7f
unknown <Ubaidillahulil@gmail.com>
2025-06-06 12:51:53 +0700

    main

---
+++ Main.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+import UASALSD.Pasien;
+
+public class Main {
+    public static Pasien inputMahasiswa(Scanner sc) {
+        System.out.print("Masukkan Nama Pasien: ");
+        String nama = sc.nextLine();
+        System.out.print("Masukkan NIK: ");
+        String nik = sc.nextLine();
+        System.out.print("Masukkan Keluhan: ");
+        String keluhan = sc.nextLine();
+        System.out.print("Masukkan IPK: ");
+        sc.nextLine();
+        return new Pasien(nik, nama, keluhan);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int pilihan;
+        do {
+            System.out.println("\n=== SISTEM ANTRIAN KLINIK ===");
+            System.out.println("1. Tambah Pasien ke Antrian");
+            System.out.println("2. Lihat Antrian");
+            System.out.println("3. Layani Pasien");
+            System.out.println("4. Cek Sisa Antrian Pasien");
+            System.out.println("5. Lihat Riwayat Transaksi");
+            System.out.println("0. Keluar");
+            System.out.print("Pilih Menu: ");
+            pilihan = sc.nextInt();
+            sc.nextLine();
+        } while (pilihan != 0);
+    }
+}
--- Pasien.java
+++ Pasien.java
@@ -2,4 +2,14 @@ package UASALSD;
 
 public class Pasien {
     
-}
+        String nama, nik, keluhan;
+    
+        public Pasien(String nama, String nik, String keluhan) {
+            this.nama = nama;
+            this.nik = nik;
+            this.keluhan = keluhan;
+        }
+        public void tampilInformasi() {
+            System.out.println("NIK " + nik + ", Nama: " + nama + ", Keluhan: " + keluhan);
+        }
+    }
