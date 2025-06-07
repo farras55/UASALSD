d44a1c262043bfcc4163d192b891e914b30fb04b
Ubaidillah1 <Ubaidillahulil32@gmail.com>
2025-06-07 14:27:38 +0700

    queue

--- Queue.java
+++ Queue.java
@@ -1,3 +1,38 @@
 public class Queue {
+    int size;
+    int front;
+    int rear;
+    int max;
+    TransaksiLayanan[] data;
+
+    public Queue(int n) {
+        max = n;
+        data = new TransaksiLayanan[max];
+        size = 0;
+        front = rear = -1;
+    }
     
+    public boolean IsEmpty() {
+        if (size == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    public boolean IsFull() {
+        if (size == max) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    
+    public Pasien layaniPasien() {
+        if (IsEmpty()) {
+            System.out.println(" Antrian kosong");
+            return null;
+        }
+        Pasien 
+    }
 }
