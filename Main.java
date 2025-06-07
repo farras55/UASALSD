6f2d597862d410595f13493882bb56768346a5e2
farras55 <farraskuyy@gmail.com>
2025-06-07 13:05:35 +0700

    Deklarasi

--- Main.java
+++ Main.java
@@ -1,7 +1,5 @@
 import java.util.Scanner;
 
-
-
 public class Main {
     public static Pasien inputMahasiswa(Scanner sc) {
         System.out.print("Masukkan Nama Pasien: ");
--- TransaksiLayanan.java
+++ TransaksiLayanan.java
@@ -1,7 +1,5 @@
 package UASALSD;
 
 public class TransaksiLayanan {
-    public static void main(String[] args) {
-        
-    }
+   
 }
