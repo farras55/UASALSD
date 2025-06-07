50f4d3217012ce83a03116b93899b713e98592a6
farras55 <farraskuyy@gmail.com>
2025-06-07 14:18:35 +0700

    class SSL

--- SslPasien.java
+++ SslPasien.java
@@ -1,3 +1,59 @@
+import java.lang.classfile.components.ClassPrinter.Node;
+
+import UASALSD.Pasien;
+
 public class SslPasien {
     NodePasien head, tail;
+    int size;
+
+    public boolean isEmpty() {
+        return head == null;
+    }
+
+    public void tambahPasien(Pasien p) {
+        NodePasien nodeBaru = new NodePasien(p);
+        if (isEmpty()) {
+            head = nodeBaru;
+            tail = nodeBaru;
+        } else {
+            tail.next = nodeBaru;
+            tail = nodeBaru;
+        }
+        System.out.println(">> Pasien masuk ke dalam antrian");
+    }
+
+    public void layaniPasien () {
+        if (isEmpty()) {
+            System.out.println("Antrian kosong.");
+        } else {
+            Pasien p = head.data;
+            head = head.next;
+            if (head == null) {
+                tail = null;
+            }
+        }
+    }
+
+    public void tampilAntrian() {
+        System.out.println();
+        if (isEmpty()) {
+            System.out.println("Antrian kosong.");
+        } else {
+            NodePasien current = head;
+            while (current != null) {
+                current.data.tampilInformasi();
+                current = current.next;
+            }
+        }
+    }
+
+    public int hitungSisa() {
+        int sisa = 0;
+        NodePasien current = head;
+        while (current != null) {
+            sisa++;
+            current = current.next;
+        }
+        return sisa;
+    }
 }
